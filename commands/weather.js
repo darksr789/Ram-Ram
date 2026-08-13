22a0741eb4f48b174521e37b8e4dfe80bc07977e
darksr789 <dyt27380@gmail.com>
2026-08-13 20:30:21 +0530

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -18,7 +18,7 @@ module.exports = {
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: "120363418906972955@newsletter",
-                        newsletterName: "𝐐α͜͡𝐝εεɼ𝐗𝐓ε𝐜𝐡",
+                        newsletterName: "ֆʊʀʏǟӼ",
                         serverMessageId: 200
                     }
                 }
@@ -51,7 +51,7 @@ module.exports = {
 💨 *Wind Speed*: ${data.wind.speed} m/s
 🔽 *Pressure*: ${data.main.pressure} hPa
 
-*© _ᴘᴏᴡᴇʀᴇᴅ ʙʏ ϙᴀᴅᴇᴇʀ-xᴅ - ᴍɪɴɪ_*
+*© _ᴘᴏᴡᴇʀᴇᴅ ʙʏ ꜱᴜʀʏᴀ-x - ᴍɪɴɪ_*
 `;
             return await sendMessageWithContext(weather);
         } catch (e) {
