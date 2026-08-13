aad7dbfedd72284bd06c8b023aa8803e20c982ec
darksr789 <dyt27380@gmail.com>
2026-08-13 20:55:59 +0530

    Update img.js

--- commands/img.js
+++ commands/img.js
@@ -20,7 +20,7 @@ module.exports = {
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
               newsletterJid: "120363418906972955@newsletter",
-              newsletterName: "𝐐α͜͡𝐝εεɼ𝐗𝐓ε𝐜𝐡",
+              newsletterName: "ֆʊʀʏǟӼ",
               serverMessageId: 200,
             },
           },
@@ -168,7 +168,7 @@ module.exports = {
             from,
             {
               image: buffer,
-              caption: `📷 Result for: ${query}\n> © *_ᴘᴏᴡᴇʀᴇᴅ ʙʏ ϙᴀᴅᴇᴇʀ-xᴅ - ᴍɪɴɪ_*`
+              caption: `📷 Result for: ${query}\n> © *_ᴘᴏᴡᴇʀᴇᴅ ʙʏ ꜱᴜʀʏᴀ-x - ᴍɪɴɪ_*`
             },
             { quoted: message }
           );
