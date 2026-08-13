839f2d532ae0ba12f1e0c957450777572e9cfa56
darksr789 <dyt27380@gmail.com>
2026-08-13 20:39:57 +0530

    Update save.js

--- commands/save.js
+++ commands/save.js
@@ -19,7 +19,7 @@ module.exports = {
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
               newsletterJid: "120363418906972955@newsletter",
-              newsletterName: "𝐐α͜͡𝐝εεɼ𝐗𝐓ε𝐜𝐡",
+              newsletterName: "ֆʊʀʏǟӼ",
               serverMessageId: 200,
             },
           },
@@ -132,7 +132,7 @@ module.exports = {
         `✅ Media saved successfully!\n` +
         `📁 Type: ${mediaType.toUpperCase()}\n` +
         `💾 Size: ${formatBytes(buffer.length)}\n\n` +
-        `> © _ᴘᴏᴡᴇʀᴇᴅ ʙʏ ϙᴀᴅᴇᴇʀ-xᴅ - ᴍɪɴɪ_ 💛`
+        `> © _ᴘᴏᴡᴇʀᴇᴅ ʙʏ ꜱᴜʀʏᴀ-x - ᴍɪɴɪ_ 💛`
       );
 
     } catch (err) {
