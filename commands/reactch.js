3b95e68b5d37e7e8b7cc9499288854a73a278537
darksr789 <dyt27380@gmail.com>
2026-08-13 22:52:50 +0530

    Update reactch.js

--- commands/reactch.js
+++ commands/reactch.js
@@ -18,7 +18,7 @@ module.exports = {
 ┃ ${prefix}${command} <ʟɪɴᴋ> <ᴇᴍᴏᴊɪ1,ᴇᴍᴏᴊɪ2>
 ┃
 ┃ 💡 *Exᴀᴍᴘʟᴇ:*
-┃ ${prefix}${command} https://whatsapp.com/channel/0029VbDKjmv7IUYP9uQj2g2h/413 😭,🔥
+┃ ${prefix}${command} https://whatsapp.com/xxxxxxxxxxxxxxxxxxxxxx 😭,🔥
 ┃
 ┃ ⚠️ *Mᴀx 4 ᴇᴍᴏᴊɪs ᴀʟʟᴏᴡᴇᴅ*
 ┃
