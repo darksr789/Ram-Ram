64949f4a4fe2ae6735edd1953ce7faa391d8a16d
darksr789 <dyt27380@gmail.com>
2026-08-13 20:48:19 +0530

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -35,7 +35,7 @@ module.exports = {
           isForwarded: true,
           forwardedNewsletterMessageInfo: {
             newsletterJid: "120363418906972955@newsletter",
-            newsletterName: "𝐐α͜͡𝐝εεɼ𝐗𝐓ε𝐜𝐡",
+            newsletterName: "ֆʊʀʏǟӼ",
             serverMessageId: 200
           }
         }
@@ -54,7 +54,7 @@ module.exports = {
           isForwarded: true,
           forwardedNewsletterMessageInfo: {
             newsletterJid: "120363418906972955@newsletter",
-            newsletterName: "𝐐α͜͡𝐝εεɼ𝐗𝐓ε𝐜𝐡",
+            newsletterName: "ֆʊʀʏǟӼ",
             serverMessageId: 200
           }
         }
